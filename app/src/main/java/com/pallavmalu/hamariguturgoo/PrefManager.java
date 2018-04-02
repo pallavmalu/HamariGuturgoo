4ddf3ab6480e31f09a55d22fe3cb3c75a803daaf
pallavmalu <pallav.30jan@gmail.com>
2018-04-02 12:40:11 +0530

    Swipe Card

---
+++ app/src/main/java/com/pallavmalu/hamariguturgoo/PrefManager.java
@@ -0,0 +1,37 @@
+package com.pallavmalu.hamariguturgoo;
+
+/**
+ * Created by pallavmalu on 31/03/18.
+ */
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "pallavmalu-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
